0e117bd73cb0ccbdbd2f46ee0d012e027b52c89a
CrosFIght <158484871+CrosFIght@users.noreply.github.com>
2024-03-13 18:00:42 +0100

    Update Password_generator.py

--- Password_generator.py
+++ Password_generator.py
@@ -9,7 +9,7 @@ i = 0
 j = int(input("How many characters your password should have: "))
 if j < 8:
     print("Password must contain at least 8 characters!")
-elif j >= 50:
+elif j > 50:
     print("The password can only have a maximum of 50 characters!")
 else:
     rand_sign = random.randint(0,25)
@@ -21,7 +21,7 @@ else:
     rand_sign = random.randint(0,9)
     sign = numbers[rand_sign]
     password = password + sign
-    while i != j - 2:
+    while i != j - 3:
         rand_sign = random.randint(0,71)
         sign = characters[rand_sign]
         password = password + sign
